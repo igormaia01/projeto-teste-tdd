71fd8d871796d61dc86718d989674207bb226a23
igormaia01 <igormaia2001@gmail.com>
2021-06-20 19:11:52 -0500

    configuraçãao inicial do projeto

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  host: '127.0.0.1',
+  username: 'postgres',
+  password: '1801',
+  database: 'tonhao',
+  dialect: 'postgres',
+  operatorAliases:false,
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredALL: true
+  }
+};
